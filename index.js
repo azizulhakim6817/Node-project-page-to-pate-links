3bc72689c4ab46b082e5cbabdaa84563ff173e5f
Azizul Hakim <148012067+azizulhakim6817@users.noreply.github.com>
2024-07-14 03:58:39 +0600

    add code

--- index.js
+++ index.js
@@ -25,33 +25,3 @@ let server = http.createServer(function (req, res) {
 
 server.listen(5050);
 console.log("successful");
-
-/* const fs = require("fs");
-const http = require("http");
-const server = http.createServer(function (req, res) {
-  if (req.url === "/") {
-    let data = fs.readFileSync("index.html", "utf8");
-    res.end(data);
-  } else if (req.url === "/about") {
-    console.log(req.url);
-
-    let data = fs.readFileSync("about.html", "utf8");
-    res.end(data);
-  } else if (req.url === "/skills") {
-    let data = fs.readFileSync("skills.html", "utf8");
-    res.end(data);
-  } else if (req.url === "/service") {
-    let data = fs.readFileSync("service.html", "utf8");
-    res.end(data);
-  } else if (req.url === "/testimonials") {
-    let data = fs.readFileSync("testimonials.html", "utf8");
-    res.end(data);
-  } else if (req.url === "/contact") {
-    let data = fs.readFileSync("contact.html", "utf8");
-    res.end(data);
-  }
-});
-
-server.listen(5050);
-console.log("server started");
- */
